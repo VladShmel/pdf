c7dc2366bc7c8378e9e387adf6c5d8b543d07392
Uladzislau_Melikhau <vlad_melichov2014@mail.ru>
2023-06-22 17:23:23 +0300

    Small fixes

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
-
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=24)
-    pdf.set_text_color(100,100,100)
+    pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0,h=12, txt=row["Topic"], align="L", ln=1)
 
     for y in range(21, 288, 10):
